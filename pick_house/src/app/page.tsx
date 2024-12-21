9ba4aa44a85c0e36bdf0142354aed9846dd080e3
JangSeung <0sunghee122@gmail.com>
2024-12-21 18:35:45 +0900

    .

--- pick_house/src/app/page.tsx
+++ pick_house/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { MoveDown } from 'lucide-react';
 
 export default function Home() {
   return (
     <div className="flex align-items-center justify-center">
-      <div className="w-full h-32 text-red-400 text-9xl">↓</div>
+      <div className="w-full h-32 text-red-400 text-9xl">
+        <MoveDown />
+      </div>
     </div>
   );
 }
