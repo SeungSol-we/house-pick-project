106e4c6875750985030385203e5d1dbd016276ae
JangSeung <0sunghee122@gmail.com>
2024-12-21 23:37:43 +0900

    .

--- pick_house/src/app/data_in_1/page.tsx
+++ pick_house/src/app/data_in_1/page.tsx
@@ -1,8 +1,15 @@
-import { CircleCheck } from 'lucide-react';
-
+import { useState } from 'react';
 import Link from 'next/link';
 
 import { Button } from "@/components/ui/button"
+import { CircleCheck } from 'lucide-react';
+
+function MyComponent() {
+  const [selectedOption, setSelectedOption] = useState('월세'); // 초기값 설정
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
 
 
 export default function Data_1() {
@@ -14,7 +21,15 @@ export default function Data_1() {
         <p className="w-full h-auto mt-2 text-xs">해당 정보를 바탕으로 회원님께 딱 맞는 집을 분석해요</p>
       
         <p className="w-full h-auto mt-4 text-base">사용될 이메일을 입력해 주세요</p>
+        <div>
+        <select value={selectedOption} onChange={handleOptionChange}>
+            <option value="월세">월세</option>
+            <option value="전세">전세</option>
+        </select>
+        <p>선택된 옵션: {selectedOption}</p>
+        </div>
       </div>
+      
 
       
     </div>
