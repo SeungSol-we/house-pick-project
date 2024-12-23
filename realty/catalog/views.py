71e8ac33eafc83f850a5a0a0ce5ec65a65d9ea95
andire120 <164429441+andire120@users.noreply.github.com>
2024-12-23 15:57:26 +0900

    s

--- realty/catalog/views.py
+++ realty/catalog/views.py
@@ -3,6 +3,19 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 import json
 
+from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.models import User
+
+class EmailBackend(BaseBackend):
+    def authenticate(self, request, email=None, password=None):
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+
+
 # 데이터 검증 및 처리 API
 def validate_and_process_user(request):
     if request.method == 'POST':
@@ -17,29 +30,21 @@ def validate_and_process_user(request):
             if action == 'register':
                 # 회원가입 처리
                 if User.objects.filter(email=email).exists():
-                    return JsonResponse({'success': False}, status=400)
+                    return JsonResponse({'success': False, 'message': "this user is no"}, status=400)
                 user = User.objects.create_user(username=username, password=password, email=email)
                 user.save()
-                return JsonResponse({'success': True}, status=201)
+                return JsonResponse({'success': True, 'message': "success sign_up"}, status=201)
 
             elif action == 'login':
                 # 로그인 처리
                 user = authenticate(request, email=email, password=password)
                 if user is not None:
                     login(request, user)
-                    return JsonResponse({'success': True}, status=200)
+                    return JsonResponse({'success': True, 'message': "success login"}, status=200)
                 else:
-                    return JsonResponse({'success': False}, status=401)
-
-            elif action == 'logout':
-                # 로그아웃 처리
-                logout(request)
-                return JsonResponse({'success': True}, status=200)
-
-            else:
-                return JsonResponse({'success': False}, status=400)
+                    return JsonResponse({'success': False, 'message': "no user or password"}, status=401)
 
         except json.JSONDecodeError:
-            return JsonResponse({'success': False}, status=400)
+            return JsonResponse({'success': False, 'message': "no function"}, status=400)
 
-    return JsonResponse({'success': False}, status=405)
+    return JsonResponse({'success': False, 'message': "no way"}, status=405)
